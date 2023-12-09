a72399f81c3e0ef692b9a46157fd58cc5fd07b58
sarnakhmel <OlSa@olsa.local>
2023-12-09 19:09:06 +0200

    styles part 1

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
+      <h1 className="text-red-500">I'm blue!</h1>
+
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
@@ -27,6 +29,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         </div>
+
       </div>
     </main>
   );
