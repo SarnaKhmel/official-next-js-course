4c73a8563803bb2745e6f6659ba7248bca2abda3
sarnakhmel <OlSa@olsa.local>
2023-12-11 19:30:07 +0200

    add image and optimaze

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana, roboto } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <h1 className={`${roboto.className} antialiased`}  >I`m blue!</h1>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -28,6 +29,9 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image src="/hero-desktop.png" alt="Hero Image" width={1000} height={760} className="hidden md:block" />
+          <Image src="/hero-mobile.png" alt="Hero Image Mobile" width={560} height={620} className="hidden md:block" />
+
         </div>
 
       </div>
