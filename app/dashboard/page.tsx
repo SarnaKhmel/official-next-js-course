a7c507573df62170cc625092aecb19ac6055fbad
sarnakhmel <OlSa@olsa.local>
2023-12-14 18:22:21 +0200

    dep

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,14 +7,13 @@ import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
 
 export default async function Page() {
     const revenue = await fetchRevenue();
-    console.log(`Revenue: ${revenue}`);
     const latestInvoices = await fetchLatestInvoices();
 
 
     return (
         <main>
-            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-                Dashboard test
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-3xl`}>
+                Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
@@ -28,8 +27,8 @@ export default async function Page() {
 
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                {/* <RevenueChart revenue={revenue} />
-                <LatestInvoices latestInvoices={latestInvoices} /> */}
+                <RevenueChart revenue={revenue} />
+                <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
     );
